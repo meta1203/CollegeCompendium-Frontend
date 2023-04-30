73e628fb711574403ace2958f1d43cc81a5a1f37
FrozenBagel <44207100+FrozenBagel@users.noreply.github.com>
2023-04-30 15:31:27 -0600

    Update HomePage

--- src/forms/HomePage.js
+++ src/forms/HomePage.js
@@ -17,7 +17,7 @@ export default function (props) {
         */
 
         <Card className="text-center">
-        <Card.Header>Ready to take your next step in life?</Card.Header>
+        <Card.Header> {user.firstName}, {user.highschool ? "are you ready to take the next step in your life?" : "are you ready to find amazing students that match your college?"} </Card.Header>
         <Card.Body>
 
         <Image
@@ -27,11 +27,11 @@ export default function (props) {
         <Card.Title>Let us help you out!</Card.Title>
         </Card.Body>
         <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src="https://images.unsplash.com/photo-1579389083078-4e7018379f7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
       <Card.Body>
         <Card.Title>Manage your profile</Card.Title>
         <Card.Text>
-          Make sure colleges have up-to-date information
+          Make sure {user.highschool ? "colleges" : "students"} have up-to-date information
         </Card.Text>
         <Button variant="primary">My Profile</Button>
       </Card.Body>
